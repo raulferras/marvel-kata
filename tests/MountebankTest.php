42b58796afa21eab41b08a46e0f33fd8cd719203
Raúl Ferràs <raulferras@gmail.com>
2020-11-05 15:28:03 +0100

    Create imposters

--- tests/MountebankTest.php
+++ tests/MountebankTest.php
@@ -2,6 +2,14 @@
 
 namespace Tests;
 
+use Demyan112rv\MountebankPHP\Imposter;
+use Demyan112rv\MountebankPHP\Mountebank;
+use Demyan112rv\MountebankPHP\Predicate;
+use Demyan112rv\MountebankPHP\Predicate\JsonPath;
+use Demyan112rv\MountebankPHP\Predicate\XPath;
+use Demyan112rv\MountebankPHP\Response;
+use Demyan112rv\MountebankPHP\Response\Behavior;
+use Demyan112rv\MountebankPHP\Stub;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 use function json_decode;
@@ -15,8 +23,11 @@ class MountebankTest extends TestCase
 
     protected function setUp(): void
     {
+        $script = '/app/tests/create.imposter.sh';
+        shell_exec($script);
+        /*
         $this->client = new Client([
-            'defaults' => ['verify' => 'false'],
+            'defaults' => ['verify' => false],
             'base_uri' => 'http://localhost:2525',  // <-- base_uri instead of base_url
         ]);
 
@@ -58,21 +69,56 @@ class MountebankTest extends TestCase
             $this->client->post('/imposters', ['json' => $body]);
         } catch (\Exception $e) {
             //...
-        }
+            throw $e;
+        }*/
+
+        /*
+        $response = new Response(Response::TYPE_IS);
+        $response->setConfig([
+            'statusCode' => 200,
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => ['foo' => 'bar']
+        ])->addBehavior(
+            (new Behavior())
+                ->setType(Behavior::TYPE_WAIT)
+                ->setConfig((new Behavior\Config\Wait())->setValue(500))
+        );
+
+        $predicate = new Predicate(Predicate::OPERATOR_EQUALS);
+        $predicate->setConfig(['path' => '/emails'])
+                  ->setXPath((new XPath())->setSelector('selector')->setNs(['foo' => 'bar']))
+                  ->setJsonPath((new JsonPath())->setSelector('selector'));
+
+
+        $stub = new Stub();
+        $stub->addResponse($response)->addPredicate($predicate);
+
+        $imposter = new Imposter();
+        $imposter->setName('Test imposter')
+                 ->setPort(1234)
+                 ->setProtocol(Imposter::PROTOCOL_HTTP)
+                 ->addStub($stub);
+
+// Mountbank config client
+        $mb = new Mountebank(new \GuzzleHttp\Client());
+        $mb->setHost('http://localhost')->setPort(2525);
+
+// Add new imposter
+        $response = $mb->addImposter($imposter);
+
+// remove all imposters
+        $response = $mb->removeImposters();*/
     }
 
     /** @test */
     public function works()
     {
-        $this->markTestSkipped('no funciona');
         $client = new Client([
-            'defaults' => ['verify' => 'false'],
-            'base_uri' => 'http://localhost:3014',  // <-- base_uri instead of base_url
+            'defaults' => ['verify' => false],
+            'base_uri' => 'http://mountebank:80',  // <-- base_uri instead of base_url
         ]);
-        $response = $client->post('/emails');
+        $response = $client->post('/emails', ['verify' => false]);
 
         $this->assertEquals('{"status":"success"}', $response->getBody()->getContents());
     }
-
-
 }
---
+++ tests/create.imposter.sh
@@ -0,0 +1,36 @@
+echo "Installing imposters...";
+curl --location --request POST 'http://mountebank:2525/imposters' \
+--header 'Content-Type: application/json' \
+--header 'Content-Type: text/plain' \
+--data-raw '{
+    "port": "80",
+    "hostname": "gateway.marvel.com",
+    "protocol": "http",
+    "name": "origin",
+    "defaultResponse": {
+        "statusCode": 404,
+        "body": "Error"
+    },
+    "stubs": [
+        {
+            "predicates": [
+                {
+                    "contains": {
+                        "method": "POST",
+                        "path": "/emails"
+                    }
+                }
+            ],
+            "responses": [
+                {
+                    "is": {
+                        "statusCode": 201,
+                        "body": {
+                            "status": "success"
+                        }
+                    }
+                }
+            ]
+        }
+    ]
+}'
